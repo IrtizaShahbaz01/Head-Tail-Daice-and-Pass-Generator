ac677361601a3a636bb50447ce48f0da60519048
Irtiza Shahbaz <shahbaz4ever17@gmail.com>
2024-10-04 11:42:58 +0500

    Add files

---
+++ app.js
@@ -0,0 +1,36 @@
+// Head and Tails 
+
+/*var userInp = prompt("Enter your decision 'Head or Tail':");
+var randomNum = Math.floor(Math.random () * 2);
+console.log(randomNum);
+if (userInp.toLowerCase() === "head") { 
+    if (randomNum === 0) {
+        alert("Heads! You win!");
+    } else {
+        alert("Tails! You lose!");
+    }
+}else if (userInp.toLowerCase() === "tail") {
+    if (randomNum === 1) {
+        alert("Tails! You win!");
+    } else {
+        alert("Heads! You lose!");
+    }
+} else{
+    alert("Invalid input! Please enter 'Head' or 'Tail'.");
+}*/
+
+
+
+
+// Daice
+
+/*var userNum = parseInt(prompt("Please enter a number between 1 to 6: "), 10);
+var ranNum = Math.floor((Math.random() * 6) + 1);
+
+if (userNum < 1 || userNum > 6 || isNaN(userNum)) {
+    alert("Invalid input! Please enter a number between 1 and 6.");
+} else if (userNum === ranNum) {
+    alert("Congratulations! You guessed correctly.");
+} else {
+    alert("Sorry, you guessed '" + userNum + "' incorrectly. The correct number was: " + ranNum);
+}*/
